8ba9f450c04f67b95e54da3ecad725b85315b406
m k <mattkleinman@Matts-MacBook-Pro.local>
2017-09-11 13:31:13 -0700

    asdad

---
+++ p2/be nice/content.js
@@ -0,0 +1,61 @@
+$('img').attr('src', 'http://i.imgur.com/Ama6kc4.png');
+
+
+$('body').children().each(function () {
+	
+	$(this).html( $(this).html().replace(/fucking/g,'<b class="blue">fuzzy</b>') );
+	$(this).html( $(this).html().replace(/fuck/g,'<b class="blue">fuzz</b>') );
+	$(this).html( $(this).html().replace(/FUCK/g,'<b class="blue">FUZZ</b>') );
+	$(this).html( $(this).html().replace(/shit/g,'<b class="blue">stuff</b>') );
+	$(this).html( $(this).html().replace(/dick/g,'<b class="blue">meanie</b>') );
+	$(this).html( $(this).html().replace(/faggot/g,'<b class="blue">NO!!!</b>') );
+	$(this).html( $(this).html().replace(/Fag/g,'<b class="blue">stop it!!</b>') );
+	$(this).html( $(this).html().replace(/fag/g,'<b class="blue">stop it!!</b>') );
+	$(this).html( $(this).html().replace(/fags/g,'<b class="blue">stop it!!</b>') );
+	$(this).html( $(this).html().replace(/bitch/g,'<b class="blue">bean</b>') );
+	$(this).html( $(this).html().replace(/Bitch/g,'<b class="blue">Bean</b>') );
+	$(this).html( $(this).html().replace(/Fuck/g,'<b class="blue">Fuzz</b>') );
+	$(this).html( $(this).html().replace(/Fucking/g,'<b class="blue">Fuzzy</b>') );
+	$(this).html( $(this).html().replace(/damn/g,'<b class="blue">darn</b>') );
+	$(this).html( $(this).html().replace(/Damn/g,'<b class="blue">Darn</b>') );
+	$(this).html( $(this).html().replace(/hell/g,'<b class="blue">heck</b>') );
+	$(this).html( $(this).html().replace(/pussy/g,'<b class="blue">fishy</b>') );
+	$(this).html( $(this).html().replace(/nigga/g,'<b class="blue">person</b>') );
+	$(this).html( $(this).html().replace(/niggas/g,'<b class="blue">people</b>') );
+	$(this).html( $(this).html().replace(/nigger/g,'<b class="blue">person</b>') );
+	$(this).html( $(this).html().replace(/niggers/g,'<b class="blue">people</b>') );
+	$(this).html( $(this).html().replace(/Niggers/g,'<b class="blue">People</b>') );
+	$(this).html( $(this).html().replace(/retard/g,'<b class="blue">nice person</b>') );
+	$(this).html( $(this).html().replace(/retards/g,'<b class="blue">nice people</b>') );
+	$(this).html( $(this).html().replace(/kike/g,'<b class="blue">friend</b>') );
+	$(this).html( $(this).html().replace(/kikes/g,'<b class="blue">friends</b>') );
+	$(this).html( $(this).html().replace(/jew/g,'<b class="blue">friend</b>') );
+	$(this).html( $(this).html().replace(/jews/g,'<b class="blue">friends</b>') );
+	//$(this).html( $(this).html().replace(/ass/g,'butt') );
+	$(this).html( $(this).html().replace(/asshole/g,'<b class="blue">mean boy</b>') );
+	$(this).html( $(this).html().replace(/assholes/g,'<b class="blue">mean boys</b>') );
+	$(this).html( $(this).html().replace(/cunt/g,'<b class="blue">cloud</b>') );
+	$(this).html( $(this).html().replace(/cunts/g,'<b class="blue">clouds</b>') );
+	$(this).html( $(this).html().replace(/cuck/g,'<b class="blue">sad :(</b>') );
+	$(this).html( $(this).html().replace(/cucks/g,'<b class="blue">sadder :(</b>') );
+	$(this).html( $(this).html().replace(/dyke/g,'<b class="blue">nice woman</b>') );
+	$(this).html( $(this).html().replace(/dykes/g,'<b class="blue">nice women</b>') );
+	$(this).html( $(this).html().replace(/sex/g,'<b class="blue">love</b>') );
+	$(this).html( $(this).html().replace(/SHOT/g,'<b class="blue">HUGGED</b>') );
+	$(this).html( $(this).html().replace(/shot/g,'<b class="blue">hugged</b>') );
+	//$(this).html( $(this).html().replace(/tit/g,'<b class="blue">balloon</b>') );
+	$(this).html( $(this).html().replace(/rape/g,'<b class="blue">very very illegal!</b>') );
+	$(this).html( $(this).html().replace(/rapist/g,'<b class="blue">gross man</b>') );
+	$(this).html( $(this).html().replace(/fingering/g,'<b class="blue">pointing</b>') );
+	$(this).html( $(this).html().replace(/moan/g,'<b class="blue">giggle</b>') );
+	$(this).html( $(this).html().replace(/kill/g,'<b class="blue">hold hands</b>') );
+	$(this).html( $(this).html().replace(/cock/g,'<b class="blue">lollipop</b>') );
+
+
+});
+
+
+
+
+
+
